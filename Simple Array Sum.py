d36f7e1c1300e514b727807d17f518036da061f5
Mohammad Ajmal Ibrahimkhail <73153620+Mohamad-Ajmal@users.noreply.github.com>
2021-04-21 14:49:54 +0430

    Add files via upload

---
+++ Simple Array Sum.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar):
+    the_sum = 0
+    for i in ar:
+        the_sum += i
+    return the_sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
